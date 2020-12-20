f8131d68d287dcebed6e970600bb43b782425555
raaynaldo <raaynaldo@hotmail.com>
2020-12-20 14:10:01 -0500

    add message model, and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+names = ["Ray", "James", "Brown", "White", "Seulgi"]
 
-User.create(username: 'Ray', password: '12345')
-User.create(username: 'James', password: '12345')
-User.create(username: 'Brown', password: '12345')
-User.create(username: 'White', password: '12345')
-User.create(username: 'Seulgi', password: '12345')
+users = names.map { |name|
+  User.create(username: name, password: "12345")
+}
+
+users.each { |user|
+  Message.create(body: "Hello, My name is #{user.username}. Nice to meet you!", user_id: user.id)
+}
