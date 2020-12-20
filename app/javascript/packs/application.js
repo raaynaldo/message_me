0efd60d8bab4a0c899ae806a4a8735f439f10d26
raaynaldo <raaynaldo@hotmail.com>
2020-12-19 20:34:12 -0500

    Install semantic ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
-// This file is automatically compiled by Webpack, along with any other files
+ // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("semantic-ui-sass")
+//= require jquery
+// Loads all Semantic javascripts
+//= require semantic-ui
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
