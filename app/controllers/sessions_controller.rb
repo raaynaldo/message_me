4a26c088966e308ac73523225154ac5203b3ebb1
raaynaldo <raaynaldo@hotmail.com>
2020-12-21 00:38:22 -0500

    add login and logout system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,28 @@
 class SessionsController < ApplicationController
-    def new
-        
+  def new
+  end
+
+  def create
+    user = User.find_by(username: session_params[:username])
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have succesfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with you login information"
+      render "new"
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have succesfully logged out"
+    redirect_to login_path
+  end
+
+  private
+
+  def session_params
+    params.require("session").permit(:username, :password)
+  end
 end
